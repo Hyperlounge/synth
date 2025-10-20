bf6fe6da5ee959f9696c12ffc3db43dfa03c93c7
Derek King <derek@hyperlounge.com>
2025-10-20 14:47:25 +0000

    Added stereo spread to delay effect

--- js/modules/DelayEffectModule.js
+++ js/modules/DelayEffectModule.js
@@ -6,14 +6,24 @@ export default class DelayEffectModule extends AudioModule {
 
         this._audioIn = new GainNode(context, {gain: 1});
         this._audioOut = new GainNode(context, {gain: 1});
-        this._delay = context.createDelay(10);
-        this._feedback = new GainNode(context, {gain: 0.5});
+        this._panner1 = new StereoPannerNode(context, {pan: 0});
+        this._delay1 = context.createDelay(10);
+        this._panner2 = new StereoPannerNode(context, {pan: 0});
+        this._delay2 = context.createDelay(10);
+        this._feedback1 = new GainNode(context, {gain: 0.5});
+        this._feedback2 = new GainNode(context, {gain: 0.5});
         this._dryLevel = new GainNode(context, {gain: 0.5});
         this._wetLevel = new GainNode(context, {gain: 0.5});
 
-        this._delay.connect(this._feedback);
-        this._delay.connect(this._wetLevel);
-        this._feedback.connect(this._delay);
+        this._delay1.connect(this._feedback1);
+        this._delay1.connect(this._panner1);
+        this._panner1.connect(this._wetLevel);
+        this._feedback1.connect(this._delay2);
+
+        this._delay2.connect(this._feedback2);
+        this._delay2.connect(this._panner2);
+        this._panner2.connect(this._wetLevel);
+        this._feedback2.connect(this._delay1);
 
         this._update();
     }
@@ -24,7 +34,7 @@ export default class DelayEffectModule extends AudioModule {
             mix: 0.5,       // 0 - 1
             time: 0.1,      // 0 - 1
             feedback: 0.5,  // 0 - 1
-            highPass: 0,    // 0 - 1
+            spread: 0,    // -1 - 1
         };
     }
 
@@ -38,13 +48,14 @@ export default class DelayEffectModule extends AudioModule {
         const mix = this._patch.get('mix');
         const time = this._patch.get('time');
         const feedback = this._patch.get('feedback');
+        const spread = this._patch.get('spread');
 
         if (this._patch.hasChanged('power')) {
             if (power) {
 
                 this._audioIn.disconnect();
 
-                this._audioIn.connect(this._delay);
+                this._audioIn.connect(this._delay1);
                 this._audioIn.connect(this._dryLevel);
                 this._dryLevel.connect(this._audioOut);
                 this._wetLevel.connect(this._audioOut);
@@ -68,8 +79,12 @@ export default class DelayEffectModule extends AudioModule {
             this._dryLevel.gain.setTargetAtTime((1-mix) * 2, this._now, this._minimumTimeConstant);
         }
 
-        this._delay.delayTime.setTargetAtTime(time*10, this._now, this._minimumTimeConstant);
-        this._feedback.gain.setTargetAtTime(feedback, this._now, this._minimumTimeConstant);
+        this._delay1.delayTime.setTargetAtTime(time*10, this._now, this._minimumTimeConstant);
+        this._delay2.delayTime.setTargetAtTime(time*10, this._now, this._minimumTimeConstant);
+        this._feedback1.gain.setTargetAtTime(feedback, this._now, this._minimumTimeConstant);
+        this._feedback2.gain.setTargetAtTime(feedback, this._now, this._minimumTimeConstant);
+        this._panner1.pan.setTargetAtTime(spread, this._now, this._minimumTimeConstant);
+        this._panner2.pan.setTargetAtTime(spread*-1, this._now, this._minimumTimeConstant);
     }
 
     get audioOut() {
